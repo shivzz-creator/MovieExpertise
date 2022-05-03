85e0184f3ca48a097f673d4de34bab00a29740e8
shivzz-creator <19ucc087@lnmiit.ac.in>
2022-05-04 00:33:54 +0530

    hover state

--- movieapp/src/components/MovieList.js
+++ movieapp/src/components/MovieList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 import { movies } from '../movieData'
 export default class MovieList extends Component {
+    constructor(){
+        super();
+        this.state={
+            hover:"",
+        };
+    }
   render() {
       let movies_array = movies.results
       console.log(movies_array)
@@ -16,7 +22,7 @@ export default class MovieList extends Component {
 <div className='movies-list'>
           {
             movies_array.map((moviesElem) => (
-              <div className="card movie-card" >
+              <div className="card movie-card"   onMouseEnter={() => this.setState({hover :moviesElem.id})} onMouseLeave={()=> this.setState({hover:""})} >
                 <img src={`https://image.tmdb.org/t/p/original${moviesElem.backdrop_path}`}
                  className="card-img-top movie-img"
                   style={{ height: '40vh', width: '20vw' }} alt="..." />
@@ -24,7 +30,12 @@ export default class MovieList extends Component {
                 <p className="card-title movie-title"><strong>{moviesElem.title}</strong></p>
 
                 <div className='button-wrapper'>
-                  <a href="#" className="btn btn-danger movie-button"><b><em>Add to Favourites</em></b></a>
+                {this.state.hover == moviesElem.id && (
+                   <a href="#" className="btn btn-danger movie-button"><b><em>Add to Favourites</em></b></a>
+                    
+                  
+                )}
+                 
                 </div>
 
 
